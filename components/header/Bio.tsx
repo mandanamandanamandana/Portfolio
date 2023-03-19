838715b90c64db8acd30cdf4d50dea7cd344d77e
Mandana <mandy7373@gmail.com>
2023-03-18 21:55:25 -0400

    Fixed Punctuation In Bio Made It String Literal

--- components/header/Bio.tsx
+++ components/header/Bio.tsx
@@ -7,38 +7,38 @@ const bio = () => {
         <h1>My Bio</h1>
         <div className={classes.bioText}>
           <p>
-            <span className={classes.bioContainerIndent}>Hi, {`&apos`} I'm Mandana,</span>{" "}
-            a passionate full-stack software developer who has recently
+            <span className={classes.bioContainerIndent}>{`Hi, I'm Mandana,`}</span>{" "}
+            {`a passionate full-stack software developer who has recently
             graduated from a highly respected programming bootcamp at General
             Assembly. While I may not have industry experience yet, I'm eager to
             apply my skills and knowledge to real-world projects and make a
-            difference for businesses of all sizes. 
+            difference for businesses of all sizes.`} 
           </p>
             <p>
 
-            <span className={classes.bioContainerIndent}>Throughout</span> my bootcamp experience, 
+            <span className={classes.bioContainerIndent}>{`Throughout`}</span>{` my bootcamp experience, 
             I gained a strong foundation in  both front-end and back-end development, 
             as well as experience with the latest programming languages and technologies. 
             I am committed to staying on the cutting edge of innovation, and I am always 
-            looking for new and exciting ways to leverage technology to drive business success.
+            looking for new and exciting ways to leverage technology to drive business success.`}
             </p>
 
             <p>
-            <span className={classes.bioContainerIndent}> Whether </span>
-            you need a new website, a custom application, or help with 
+            <span className={classes.bioContainerIndent}>{` Whether `}</span>
+            {`you need a new website, a custom application, or help with 
             optimizing your existing digital presence, I am ready to tackle any
             challenge and deliver exceptional results. With a focus on quality,
             attention to detail, and a dedication to excellence, I am committed
             to helping you achieve your digital goals and take your business to
-            the next level. 
+            the next level. `}
             </p>
 
             <p>
-            <span className={classes.bioContainerIndent}>I'm </span> 
-            excited to start my career in the industry and to work with 
+            <span className={classes.bioContainerIndent}>{`I'm`} </span> 
+            {`excited to start my career in the industry and to work with 
             you to create innovative solutions that make a real
             difference. Get in touch today to learn more about how I can help
-            you succeed!
+            you succeed!`}
             </p>
         </div>
       </div>
